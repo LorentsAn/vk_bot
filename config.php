921fe8090590a0b33331300d88c77cbcef0a0d53
Lorents_Anna <312219@niuitmo.ru>
2022-12-11 15:29:34 +0300

    make config and index file

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
 
-define('CALLBACK_API_CONFIRMATION_TOKEN', '3e018bc9'); //Строка для подтверждения адреса сервера из настроек Callback API
+define('CALLBACK_API_CONFIRMATION_TOKEN', 'b367b6bf'); //Строка для подтверждения адреса сервера из настроек Callback API
 define('VK_API_ACCESS_TOKEN', 'vk1.a.anFJUNDJNlluKss_oAYbQPACHtU3VZxIf2CiJ6pP18oI_Msp3zL-V0uBn6FiO4DgsN8mg54-Hho7pz6H7r9f1e90eHlvn-oUlP0a2NzRGWh9Z8oy5B7HROG_iRL9kacHpzgh4B6fUPM7QNV5KAxgioWBFZk96K9zqoqJ-8z5dX4sCatJDrZrVEA5E5_jOVqp-WLHM8Dr-SdeYu-Oow3i9w'); //Ключ доступа сообщества
